6ee70478644a18ebb6d5d7244d580ce832f36d56
Naimish Kumar Verma <94442873+Naimishkumar1@users.noreply.github.com>
2023-10-06 11:47:06 +0530

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
     return BlocProvider(
       create: (context) => BlogCubit(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
           title: 'Flutter Demo',
           theme: ThemeData(
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
